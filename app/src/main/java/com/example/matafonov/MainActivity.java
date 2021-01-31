e8c887d18181e34914c83497c3fe3371ae8e6db7
Denis <dmatafonov2000@mail.ru>
2021-01-31 22:48:45 +0500

    Запрещена смена ориентации

--- app/src/main/java/com/example/matafonov/MainActivity.java
+++ app/src/main/java/com/example/matafonov/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.matafonov;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import com.example.matafonov.utils.TypefaceUtil;
@@ -22,13 +23,6 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         TypefaceUtil.overrideFont(getApplicationContext(), "sans", "fonts/Nunito-Regular.ttf");
-
-        /*fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
